b7965261d51d8d4f53e1a4dd40b862f76ac71fda
ESukmean <esukmean@esukmean.com>
2022-02-13 20:28:12 +0900

    기본적인 테그 검색시스템 추가

--- collecter/crawler/__init__.py
+++ collecter/crawler/__init__.py
@@ -1,24 +1,50 @@
 from datetime import datetime
 from typing import *
+import re
+
+_tag_keyword_re = {
+	'network': re.compile('network|ip|packet'),
+	'web': re.compile('html|css|javascript|js'),
+	'android': re.compile('android|안드로이드'),
+	'ios': re.compile('iphone|ipad|ios')
+}
+_strip_tag_regex = re.compile('<.*?>')
 
 class article:
-	title: str
-	article: str
-	post: datetime
-	author: str
-	url: str
-	category: List[str]
-	tag: Tuple[str]
+	title: str = ''
+	article: str = ''
+	post: datetime = None
+	author: str = ''
+	url: str = ''
+	category: List[str] = []
+	tag: Tuple[str] = ()
 
 	def __init__(self, **kwargs) -> None:
 		self.__dict__.update(kwargs)
-		
+	
 	def get_summary(self) -> str:
-		pass
+		if 'article' not in self.__dict__:
+			return ''
+
+		article = _strip_tag_regex.sub('', self.article)
+		import html
+
+		return html.unescape(article)
 
 	def get_tag(self) -> Tuple[str]:
-		pass
-	
+		result = []
+
+		if 'category' in self.__dict__:
+			result.extend(self.category)
+		
+		article = self.get_summary()
+		for tag, regex in _tag_keyword_re.items():
+			if regex.find(article) == -1:
+				continue
+				
+			result.append(tag)
+
+		return tuple(result)
 	
 class blog:
 	blog_id: int
@@ -29,10 +55,16 @@ class blog:
 	article: List[article]
 
 	def __init__(self, **kwargs):
+		self.blog_id = 0
+		self.name = ''
+		self.link = ''
+		self.description = ''
+		self.article = list()
+
 		self.__dict__.update(kwargs)
 		
 	async def fetch(self) -> List[article]:
-		pass
+		return []
 
-	async def add_article(self, article: 'article'):
+	def add_article(self, article: 'article'):
 		self.article.append(article)
